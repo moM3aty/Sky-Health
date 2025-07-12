610465c5a620f6a71e416c734c20bb32dfcf3a88
moM3aty <mo.m3aty@gmail.com>
2025-07-12 21:22:44 +0300

    fix some issues

--- Sky Health/Migrations/20250712175218_addIsNew.Designer.cs
+++ Sky Health/Migrations/20250712175218_addIsNew.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sky_Health.Data;
 
@@ -11,9 +12,11 @@ using Sky_Health.Data;
 namespace Sky_Health.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250712175218_addIsNew")]
+    partial class addIsNew
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
